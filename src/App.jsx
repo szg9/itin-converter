fed03f0798fec3454d88f765e9c76085e154d18f
Szász Gergő <szaszgergo9@gmail.com>
2022-06-28 14:26:14 +0200

    design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { useState } from 'react';
 import airports from './db/airports.json';
 
@@ -189,11 +189,18 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>ITINConverter</h1>
-      <textarea onChange={handleInputChange} name="input" id="input" cols="80" rows="20"></textarea>
-      <button onClick={handleButtonClick} name="button" id="button">Magic</button>
-      <div>{inputValue}</div>
+    <div className='container'>
+      <h1><span>ITIN</span>Converter</h1>
+      <div className='form-group'>
+        <textarea onChange={handleInputChange} name="input" id="input" cols="100" rows="10" className='form-control' placeholder="Másold be az itinerary-t Amadeusból..."></textarea>
+      </div>
+      <button onClick={handleButtonClick} name="button" id="button" className="btn btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-magic" viewBox="0 0 16 16">
+          <path d="M9.5 2.672a.5.5 0 1 0 1 0V.843a.5.5 0 0 0-1 0v1.829Zm4.5.035A.5.5 0 0 0 13.293 2L12 3.293a.5.5 0 1 0 .707.707L14 2.707ZM7.293 4A.5.5 0 1 0 8 3.293L6.707 2A.5.5 0 0 0 6 2.707L7.293 4Zm-.621 2.5a.5.5 0 1 0 0-1H4.843a.5.5 0 1 0 0 1h1.829Zm8.485 0a.5.5 0 1 0 0-1h-1.829a.5.5 0 0 0 0 1h1.829ZM13.293 10A.5.5 0 1 0 14 9.293L12.707 8a.5.5 0 1 0-.707.707L13.293 10ZM9.5 11.157a.5.5 0 0 0 1 0V9.328a.5.5 0 0 0-1 0v1.829Zm1.854-5.097a.5.5 0 0 0 0-.706l-.708-.708a.5.5 0 0 0-.707 0L8.646 5.94a.5.5 0 0 0 0 .707l.708.708a.5.5 0 0 0 .707 0l1.293-1.293Zm-3 3a.5.5 0 0 0 0-.706l-.708-.708a.5.5 0 0 0-.707 0L.646 13.94a.5.5 0 0 0 0 .707l.708.708a.5.5 0 0 0 .707 0L8.354 9.06Z" />
+        </svg>
+        <p>Magic</p>
+      </button>
+      <div className='result'>{inputValue}</div>
     </div>
 
   );
